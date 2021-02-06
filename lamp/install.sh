05913521844e2bd42d4771bb1e497be60de9e586
Musa Sutisna <mus4.sutisn4@gmail.com>
2021-02-06 09:25:35 +0700

    Dont rename custom php ini on installing for lamp

--- lamp/install.sh
+++ lamp/install.sh
@@ -7,8 +7,5 @@ mv ./php-apache/etc-example ./php-apache/etc
 # Set php apache environment
 mv ./php-apache/.env.example ./php-apache/.env
 
-# Set php custom configuration
-mv ./php-apache/custom.ini.example ./php-apache/custom.ini
-
 # You got installed so don't need me after that!
 rm install.sh
