cdee2ec8d5c30f1299a904c34aebdc4f2d23bd28
Musa Sutisna <mus4.sutisn4@gmail.com>
2021-03-23 21:23:21 +0700

    Replace nodejs CMD with entrypoint for mean

--- mean/install.sh
+++ mean/install.sh
@@ -5,5 +5,8 @@ cp ./mongodb/mongod.log.example ./mongodb/mongod.log
 # Set app environment
 cp ./app/.env.example ./app/.env
 
+# Set nodejs entrypoint
+cp ./app/entrypoint.sh.example ./app/entrypoint.sh
+
 # You got installed so don't need me after that!
 rm install.sh
