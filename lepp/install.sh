c5a690995c7a3f37c2856462642869ee73d858dc
Musa Sutisna <mus4.sutisn4@gmail.com>
2021-09-12 19:52:00 +0700

    Lepp using example to config file nginx

--- lepp/install.sh
+++ lepp/install.sh
@@ -1,6 +1,9 @@
 # Set postgres environment
 mv ./postgres/.env.example ./postgres/.env
 
+# Set nginx configuration
+cp ./nginx/conf.d/default.conf.example ./nginx/conf.d/default.conf
+
 # Set php environment
 mv ./php/.env.example ./php/.env
 
