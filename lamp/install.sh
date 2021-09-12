1310eb0cadf099159db4b63d92f1b028a4258ec3
Musa Sutisna <mus4.sutisn4@gmail.com>
2021-09-12 19:53:54 +0700

    Use cp and dont remove install sheel script

--- lamp/install.sh
+++ lamp/install.sh
@@ -1,11 +1,11 @@
 # Set mariadb environment
-mv ./mariadb/.env.example ./mariadb/.env
+cp ./mariadb/.env.example ./mariadb/.env
 
 # Set apache virtual host directory
-mv ./php-apache/etc-example ./php-apache/etc
+cp ./php-apache/etc-example ./php-apache/etc
 
 # Set php apache environment
-mv ./php-apache/.env.example ./php-apache/.env
+cp ./php-apache/.env.example ./php-apache/.env
 
 # You got installed so don't need me after that!
-rm install.sh
+# rm install.sh
--- lemp/install.sh
+++ lemp/install.sh
@@ -1,11 +1,11 @@
 # Set mariadb environment
-mv ./mariadb/.env.example ./mariadb/.env
+cp ./mariadb/.env.example ./mariadb/.env
 
 # Set nginx configuration
 cp ./nginx/conf.d/default.conf.example ./nginx/conf.d/default.conf
 
 # Set php environment
-mv ./php/.env.example ./php/.env
+cp ./php/.env.example ./php/.env
 
 # You got installed so don't need me after that!
-rm install.sh
+# rm install.sh
