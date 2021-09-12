36cb6e679cccd4a8a16ad0720993073d3de22b32
Musa Sutisna <mus4.sutisn4@gmail.com>
2021-09-12 19:49:53 +0700

    Lepp remove logs from nginx default file

--- lepp/install.sh
+++ lepp/install.sh
@@ -1,10 +1,6 @@
 # Set postgres environment
 mv ./postgres/.env.example ./postgres/.env
 
-# Set nginx logs
-mv ./nginx/logs/access.log.example ./nginx/logs/access.log
-mv ./nginx/logs/error.log.example ./nginx/logs/error.log
-
 # Set php environment
 mv ./php/.env.example ./php/.env
 
